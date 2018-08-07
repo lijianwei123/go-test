05b253bfd1ed01d910dcfd6697371ad8e0fd96df
李建伟 <lijianwei_123@126.com>
2018-08-07 16:49:13 +0800

    add id_gen

--- id-gen/handlers.go
+++ id-gen/handlers.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"net/http"
 	"strconv"
 )
 
 func requestID(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	key := r.Form.Get("key")
-	result := Incr(key)
+	//r.ParseForm()
+	//key := r.Form.Get("key")
+	//result := Incr(key)
+
+	uuid.New().String()
+	//fmt.Println(uuid)
+	result := true
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(strconv.FormatBool(result)))
